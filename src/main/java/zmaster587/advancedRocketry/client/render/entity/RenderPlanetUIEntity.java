3062e89fd268553af26df0ede8dd65c99f3cfceb
StargateMC <draksterau@gmail.com>
2020-06-19 17:41:35 +1000

    WIP

--- src/main/java/zmaster587/advancedRocketry/client/render/entity/RenderPlanetUIEntity.java
+++ src/main/java/zmaster587/advancedRocketry/client/render/entity/RenderPlanetUIEntity.java
@@ -210,9 +210,9 @@ public class RenderPlanetUIEntity extends Render<EntityUIPlanet> implements IRen
 
 			//Render planet name
 			RenderHelper.cleanupPlayerFacingMatrix();
-			RenderHelper.renderTag(Minecraft.getMinecraft().player.getDistanceSq(hitObj.hitVec.z, hitObj.hitVec.y, hitObj.hitVec.x), properties.getName(), 0, .9, 0, 5);
+			RenderHelper.renderTag(Minecraft.getMinecraft().player.getDistanceSq(hitObj.hitVec.z, hitObj.hitVec.y, hitObj.hitVec.x), properties.getName(), 0, .15, 0, 5);
 			RenderHelper.renderTag(Minecraft.getMinecraft().player.getDistanceSq(hitObj.hitVec.z, hitObj.hitVec.y, hitObj.hitVec.x), "NumMoons: " + properties.getChildPlanets().size(), 0, .6, 0, 5);
-			RenderHelper.renderTag(Minecraft.getMinecraft().player.getDistanceSq(hitObj.hitVec.x, hitObj.hitVec.y, hitObj.hitVec.z), "Distance: " + properties.stellarDistanceFrom(entity.world, entity.getPosition()) + " SDU (" + TileWarpShipMonitor.getEnglishTimeFromMs(((long)properties.stellarDistanceFrom(entity.world, entity.getPosition()) * 5000)) + ")", 0, .12, 0, 5);
+			RenderHelper.renderTag(Minecraft.getMinecraft().player.getDistanceSq(hitObj.hitVec.x, hitObj.hitVec.y, hitObj.hitVec.z), "Distance: " + (Math.round(properties.stellarDistanceFrom(entity.world, entity.getPosition()))) + " SDU (" + TileWarpShipMonitor.getEnglishTimeFromMs(((long)properties.stellarDistanceFrom(entity.world, entity.getPosition()) * 5000)) + ")", 0, .12, 0, 5);
 
 			GL11.glPopMatrix();
 		}
