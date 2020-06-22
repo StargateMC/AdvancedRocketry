e02f897c05128191d7ea671c3f1aafbf97267884
StargateMC <draksterau@gmail.com>
2020-06-22 13:26:21 +1000

    WIP

--- src/main/java/zmaster587/advancedRocketry/tile/station/TilePlanetaryHologram.java
+++ src/main/java/zmaster587/advancedRocketry/tile/station/TilePlanetaryHologram.java
@@ -156,7 +156,7 @@ public class TilePlanetaryHologram extends TileEntity implements ITickable,IButt
                                                                 equivZ += 500;
                                                             }
                                                             try {
-                                                                equivY += Math.max(TileWarpShipMonitor.distanceBetweenStars(this.getCurrentStar(),entity.getStarProperties()) * 0.0001,8);
+                                                                equivY += TileWarpShipMonitor.distanceBetweenStars(this.getCurrentStar(),entity.getStarProperties()) * 0.00001;
                                                             } catch (Exception e) {
                                                                 equivY += 30;
                                                             }
